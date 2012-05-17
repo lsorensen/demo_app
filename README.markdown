b0c1f65276218896a853428d6c0571a2edbfd458
Luke Sorensen <luke.sorensen@jds.net.au>
2012-05-17 10:34:22 +1000

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+This is the first demo (interactive) application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'aacdb17d2e2cc95430e45475d8865828a58773a6facaf0cde90a1cb7824ca4337f3962518cba085fa94d7ea2aa36ffee6d270f88c260a57cc654437c49567d00'
